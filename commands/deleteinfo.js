deb50102ef6ada09d546d6056c2fd20e4617964d
xiko <borna5krpan@gmail.com>
2025-07-22 20:57:38 +0200

    Added deleteinfo.js

---
+++ commands/deleteinfo.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const User = require('../models/User');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('deleteinfo')
+        .setDescription('Delete your timezone information'),
+
+    async execute(interaction) {
+        const userId = interaction.user.id;
+
+        try {
+            await User.deleteOne(
+                { userId },
+            );
+
+            return interaction.reply({
+                content: `✅ Your information has been deleted.`,
+                flags: MessageFlags.Ephemeral
+            });
+        } catch (err) {
+            console.error(err);
+            return interaction.reply({
+                content: '⚠️ Failed to delete user information.',
+                flags: MessageFlags.Ephemeral
+            });
+        }
+    }
+};
